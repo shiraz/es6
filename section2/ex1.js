23ad18fd6f1e874fe07a18829eee3896ed050870
mirzasb <mirzasbaig@nyu.edu>
2022-05-25 00:04:08 -0400

    Section 2 complete

---
+++ section2/ex1.js
@@ -0,0 +1,18 @@
+/*
+The code below is calling 'savePost' three times, but it is doing so using a for loop.  
+This implementation works, but the for loop makes it more challenging to understand the purpose of the function.  
+Rather than using a for loop, refactor the code below to instead use the forEach helper.
+*/
+
+function handlePosts() {
+    var posts = [
+      { id: 23, title: 'Daily JS News' },
+      { id: 52, title: 'Code Refactor City' },
+      { id: 105, title: 'The Brightest Ruby' }
+    ];
+    
+    // for (var i = 0; i < posts.length; i++) {
+    //   savePost(posts[i]);
+    // }
+    posts.forEach(savePost);
+}
