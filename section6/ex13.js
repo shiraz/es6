b938ad3cb198747f563f97299394d019301ff1f2
mirzasb <mirzasbaig@nyu.edu>
2022-06-05 10:53:44 -0400

    Section 6 complete

---
+++ section6/ex13.js
@@ -0,0 +1,12 @@
+/*
+Given an array of network objects representing network requests, assign the boolean 'true' to 
+the variable 'inProgress' if any network request has a 'status' of 'pending'.
+*/
+
+var requests = [
+  { url: '/photos', status: 'complete' },
+  { url: '/albums', status: 'pending' },
+  { url: '/users', status: 'failed' },
+];
+
+var inProgress = requests.some((req) => req.status === 'pending');
